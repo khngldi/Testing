6924f65b6938dc9a0de665fc9315423800624ce2
Khangeldi Shakhmukhanbetov <khanekshakh@gmail.com>
2025-05-07 23:11:33 +0500

    CollectStatic

--- Freshmart/settings.py
+++ Freshmart/settings.py
@@ -160,9 +160,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Для сбора статики в продакшене
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),  # Путь к вашей папке static
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
